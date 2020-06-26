1baf094e04c97aecfab492b08345e8552d4ce1c1
Tyisha Williams <tlwilliams895@gmail.com>
2020-06-25 20:42:27 -0400

    assessment ready

--- src/main.js
+++ src/main.js
@@ -2,16 +2,35 @@ let boxTop = 200;
 let boxLeft = 200;
 
 document.addEventListener('keydown', logKey);
+//If-Else-If Statement
+// function logKey(e) {
+//     log.textContent += ` ${e.code}`;
+//     if (e.code === "ArrowUp") {
+//         document.getElementById("box").style.top = (boxTop -= 10) + "px";
+//     } else if (e.code === "ArrowDown") {
+//         document.getElementById("box").style.top = (boxTop += 10) + "px";
+//     } else if (e.code === "ArrowLeft") {
+//         document.getElementById("box").style.left = (boxLeft -= 10) + "px";
+//     } else if (e.code === "ArrowRight") {
+//         document.getElementById("box").style.left = (boxLeft += 10) + "px";
+//     }
+// }
 
+// Switch Statement
 function logKey(e) {
     log.textContent += ` ${e.code}`;
-    if (e.code === "ArrowUp") {
-        document.getElementById("box").style.top = (boxTop -= 10) + "px";
-    } else if (e.code === "ArrowDown") {
-        document.getElementById("box").style.top = (boxTop += 10) + "px";
-    } else if (e.code === "ArrowLeft") {
-        document.getElementById("box").style.left = (boxLeft -= 10) + "px";
-    } else if (e.code === "ArrowRight") {
-        document.getElementById("box").style.left = (boxLeft += 10) + "px";
+    switch (e.code) {
+        case "ArrowUp":
+            document.getElementById("box").style.top = (boxTop -= 10) + "px";
+            break;
+        case "ArrowDown":
+            document.getElementById("box").style.top = (boxTop += 10) + "px";
+            break;
+        case "ArrowRight":
+            document.getElementById("box").style.left = (boxLeft -= 10) + "px";
+            break;
+        case "ArrowLeft":
+            document.getElementById("box").style.left = (boxLeft += 10) + "px";
+            break;
     }
 }
